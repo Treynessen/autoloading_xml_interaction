7a187fa3e6b76fd2850c03d51684be973a648a94
Treynessen <trane5871@gmail.com>
2021-04-13 22:04:17 +0200

    Доделанный шаблон регекса

---
+++ autoliading_xml_interaction/Ad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace autoliading_xml_interaction
+{
+    public class Ad
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public DateTime EndDate { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
--- autoliading_xml_interaction/YandexXmlParser/ExtractAdsXmlContent.cs
+++ autoliading_xml_interaction/YandexXmlParser/ExtractAdsXmlContent.cs
@@ -6,18 +6,45 @@ namespace autoliading_xml_interaction
 {
     partial class YandexXmlParser
     {
-        //Делим полученный xml файл с объявлениями на список с объявлениями
-        private LinkedList<string> ExtractAdsXmlContent()
+        /// <summary>
+        /// Парсинг переданного в конструктор xmlFileContent: извлечение значений для свойств объекта Ad
+        /// </summary>
+        private LinkedList<Ad> ExtractAdsXmlContent()
         {
-            Regex regex = new Regex(@"<offer>([\s\S]+?)</offer>");
-            Match match = regex.Match(xmlFileContent);
-            LinkedList<string> adsList = new LinkedList<string>();
-            while (match.Success)
+            Regex regex = new Regex(
+                @"<offer>\s{0,2}" +
+                    @"<!-- \[CATEGORY_ID=(?<CATEGORY_ID>(ID\d+))\] -->\s{0,2}" + // Извлечение CategoryId из <!-- [CATEGORY_ID=идентификатор] -->
+                    @"<!-- \[END_DATE=(?<END_DATE>(\d{4}\-\d{2}\-\d{2}))\] -->\s{0,2}" + // Извлечение EndDate из <!-- [END_DATE=дата] -->
+                    @"<id>(?<AD_ID>(ID\d+))</id>" + // Извлечение Id из <id>идентификатор</id>
+                    @"[\S\s]+?" + // промежуточные строки
+                    @"<title>(?<AD_NAME>(.+))</title>\s{0,2}" + // Извлечение Name из <title>имя</title>
+                    @"<description><!\[CDATA\[\s{0,4}" + // <description><![CDATA[
+                        @"(?<DESCRIPTION>([\S\s]+?))" + // Извлечение Description из <description><![CDATA[описание]]></description>
+                    @"\s{0,4}\]\]></description>\s{0,2}" + // ]]></description>
+                    @"[\S\s]+?" + // промежуточные строки
+                    @"<price>(?<PRICE>(\d+))</price>\s{0,2}" + // Извлечение Price из <price>цена</price>" +
+                "</offer>"
+            );
+            MatchCollection matches = regex.Matches(xmlFileContent);
+            LinkedList<Ad> adList = new LinkedList<Ad>();
+            foreach (Match match in matches)
             {
-                adsList.AddLast(match.Value);
-                match = match.NextMatch();
+                Ad ad = new Ad
+                {
+                    Id = match.Groups["AD_ID"].Value,
+                    Name = match.Groups["AD_NAME"].Value,
+                    Description = match.Groups["DESCRIPTION"].Value,
+                    Price = Convert.ToInt32(match.Groups["PRICE"].Value),
+                    CategoryId = match.Groups["CATEGORY_ID"].Value
+                };
+                string[] splitedEndDate = match.Groups["END_DATE"].Value.Split('-');
+                ad.EndDate = new DateTime(
+                    year: Convert.ToInt32(splitedEndDate[0]),
+                    month: Convert.ToInt32(splitedEndDate[1]),
+                    day: Convert.ToInt32(splitedEndDate[2])
+                );
             }
-            return adsList;
+            return adList;
         }
     }
 }
